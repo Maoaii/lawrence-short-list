8260338cecab70e3fa75b6da8a71148af8736988
Girotto <l.girottoph@gmail.com>
2026-05-12 23:04:32 +0100

    gravity component implemented

--- components/movement/gravity_component.gd
+++ components/movement/gravity_component.gd
@@ -1,1 +1,17 @@
+class_name GravityComponent
 extends Node
+
+@export var gravity: float = 980.0
+@export var fall_multiplier: float = 1.6
+@export var jump_gravity_scale: float = 0.6
+
+var _is_jumping: bool = false
+
+func set_jumping(value: bool) -> void:
+    _is_jumping = value
+
+func apply(body: CharacterBody2D, delta: float) -> void:
+    if body.is_on_floor():
+        return
+    var scale := jump_gravity_scale if _is_jumping and body.velocity.y < 0.0 else fall_multiplier
+    body.velocity.y += gravity * scale * delta
