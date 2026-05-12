80b5fc1f16cfc633956377562eaf02644d194887
Girotto <l.girottoph@gmail.com>
2026-05-12 23:04:55 +0100

    player input component implemented

--- components/movement/player_input_component.gd
+++ components/movement/player_input_component.gd
@@ -1,1 +1,11 @@
+class_name PlayerInputComponent
 extends Node
+
+func get_move_direction() -> float:
+    return Input.get_axis("ui_left", "ui_right")
+
+func is_jump_pressed() -> bool:
+    return Input.is_action_just_pressed("ui_accept")
+
+func is_jump_held() -> bool:
+    return Input.is_action_pressed("ui_accept")
