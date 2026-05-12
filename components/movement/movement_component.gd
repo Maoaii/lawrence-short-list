4f64bf5db39933fc4be0f4978551e552de9d530b
Girotto <l.girottoph@gmail.com>
2026-05-12 23:04:13 +0100

    movement component implementation

--- components/movement/movement_component.gd
+++ components/movement/movement_component.gd
@@ -1,1 +1,14 @@
+class_name MovementComponent
 extends Node
+
+@export var speed: float = 200.0
+@export var acceleration: float = 800.0
+@export var friction: float = 1200.0
+
+func apply(body: CharacterBody2D, direction: float, delta: float) -> void:
+    if direction != 0.0:
+        body.velocity.x = move_toward(
+            body.velocity.x, direction * speed, acceleration * delta
+        )
+    else:
+        body.velocity.x = move_toward(body.velocity.x, 0.0, friction * delta)
