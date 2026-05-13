600af95275c0395ccc9b0a21f9154749590aed32
Girotto <l.girottoph@gmail.com>
2026-05-13 07:00:07 +0100

    final components implemented

--- components/abilities/bounce.gd
+++ components/abilities/bounce.gd
@@ -1,1 +1,14 @@
+class_name BounceComponent
 extends Node
+
+signal bounced
+
+@export var bounce_force: float = 700.0
+@export var enabled: bool = true
+
+func try_bounce(body: CharacterBody2D) -> bool:
+    if not enabled:
+        return false
+    body.velocity.y = -bounce_force
+    bounced.emit()
+    return true
--- components/movement/player_input_component.gd
+++ components/movement/player_input_component.gd
@@ -1,11 +1,18 @@
 class_name PlayerInputComponent
 extends Node
 
+@export var left_action: String = "left"
+@export var right_action: String = "right"
+@export var jump_action: String = "jump"
+
 func get_move_direction() -> float:
-    return Input.get_axis("ui_left", "ui_right")
+    return Input.get_axis(left_action, right_action)
 
 func is_jump_pressed() -> bool:
-    return Input.is_action_just_pressed("ui_accept")
+    return Input.is_action_just_pressed(jump_action)
 
 func is_jump_held() -> bool:
-    return Input.is_action_pressed("ui_accept")
+    return Input.is_action_pressed(jump_action)
+
+func is_jump_released() -> bool:
+    return Input.is_action_just_released(jump_action)
