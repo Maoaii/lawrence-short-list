bd33ec8190bd41a739be8088aebe2cbbdfaff266
Girotto <l.girottoph@gmail.com>
2026-05-13 07:00:38 +0100

    player and world set up as playground

--- player/player.gd
+++ player/player.gd
@@ -1,1 +1,20 @@
 extends CharacterBody2D
+
+@export var jump: JumpComponent
+@export var state_machine: StateMachine
+@export var bounce: BounceComponent
+
+func _ready() -> void:
+	if not jump:
+		push_error("Player is missing a JumpComponent.")
+		return
+	if not state_machine:
+		push_error("Player is missing a StateMachine.")
+		return
+	if not bounce:
+		push_error("Player is missing a BounceComponent.")
+		return
+
+	bounce.bounced.connect(func():
+		state_machine.transition_to(state_machine.get_node("Jump"))
+	)
--- utils/logger.gd
+++ utils/logger.gd
@@ -1,11 +1,12 @@
 extends Node
 
 func log(message: String, message_owner: Node, debug: bool) -> void:
-    if not debug:
-        return
-    
-    var timestamp = Time.get_time_dict_from_system().hour + ":" + \
-        Time.get_time_dict_from_system().minute + ":" + \
-        Time.get_time_dict_from_system().second
-    
-    print("[" + timestamp + "] [" + message_owner.name + "]: " + message)
+	if not debug:
+		return
+	
+	var time_dict = Time.get_time_dict_from_system()
+	var timestamp = str(time_dict.hour) + ":" + \
+		str(time_dict.minute) + ":" + \
+		str(time_dict.second)
+	
+	print("[" + timestamp + "] [" + message_owner.name + "]: " + message)
